e743e49d8133d9811076944815b556f8d5a8a5ca
cvUPTZ <cvupdz@gmail.com>
2025-03-05 10:24:20 +0100

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "";
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://pcbvdnudargsvtdlrium.supabase.co";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjYnZkbnVkYXJnc3Z0ZGxyaXVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExNjUxMzIsImV4cCI6MjA1Njc0MTEzMn0.XjWNpDe92oD4aTvxAFZnh8gkCSF2VNBghFxcDGGAaEE";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
